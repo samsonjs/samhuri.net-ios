1735cfacdb86e0a745d3ed84b9d00deb6dc212cb
Sami Samhuri <sami@samhuri.net>
2015-05-09 18:36:03 -0700

    s/UIWebView/WKWebView/ and add another activity indicator

--- Blog/PreviewViewController.m
+++ Blog/PreviewViewController.m
@@ -6,34 +6,57 @@
 //  Copyright (c) 2015 Guru Logic Inc. All rights reserved.
 //
 
+@import WebKit;
 #import "PreviewViewController.h"
 
-@interface PreviewViewController () <UIWebViewDelegate>
+@interface PreviewViewController () <WKNavigationDelegate>
 
-@property (nonatomic, weak) IBOutlet UIWebView *webView;
-@property (nonatomic, weak) IBOutlet UIActivityIndicatorView *indicatorView;
+@property (nonatomic, weak) IBOutlet WKWebView *webView;
+@property (nonatomic, weak) IBOutlet UIActivityIndicatorView *centerIndicatorView;
+@property (nonatomic, weak) UIActivityIndicatorView *cornerIndicatorView;
 
 @end
 
 @implementation PreviewViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupCornerIndicatorView];
+    [self setupWebView];
+}
+
+- (void)setupCornerIndicatorView {
+    UIActivityIndicatorView *indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    indicator.hidesWhenStopped = YES;
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:indicator];
+    self.cornerIndicatorView = indicator;
+}
+
+- (void)setupWebView {
+    WKWebViewConfiguration *configuration = [WKWebViewConfiguration new];
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:CGRectZero configuration:configuration];
+    webView.translatesAutoresizingMaskIntoConstraints = NO;
+    webView.navigationDelegate = self;
+    [self.view insertSubview:webView belowSubview:self.centerIndicatorView];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:webView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:webView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:webView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeWidth multiplier:1 constant:0]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:webView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeHeight multiplier:1 constant:0]];
+    self.webView = webView;
+}
+
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-
-    // UIWebView restores its request so we just have to reload it
-    if (!self.initialRequest && self.webView.request) {
-        [self.webView reload];
-        [self.indicatorView startAnimating];
-        return;
-    }
 
     if (self.initialRequest) {
         PMKPromise *p = self.promise ?: [PMKPromise promiseWithValue:nil];
         __weak typeof(self) welf = self;
         p.then(^{
             typeof(self) self = welf;
+            [self.centerIndicatorView startAnimating];
+            [self.cornerIndicatorView stopAnimating];
+            self.webView.hidden = YES;
             [self.webView loadRequest:self.initialRequest];
-            [self.indicatorView startAnimating];
         }).finally(^{
             typeof(self) self = welf;
             self.promise = nil;
@@ -42,23 +65,31 @@
     }
 }
 
-- (void)setInitialRequest:(NSURLRequest *)initialRequest {
-    _initialRequest = initialRequest;
+- (void)applicationFinishedRestoringState {
+    [super applicationFinishedRestoringState];
+    [self.centerIndicatorView startAnimating];
+    [self.cornerIndicatorView stopAnimating];
     self.webView.hidden = YES;
+    [self.webView reload];
 }
 
 #pragma mark - UIWebViewDelegate methods
 
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
-    self.webView.hidden = NO;
-    [self.indicatorView stopAnimating];
-    if ([webView.request.URL isEqual:self.initialRequest.URL]) {
-        self.initialRequest = nil;
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+    if (!self.webView.hidden) {
+        [self.cornerIndicatorView startAnimating];
     }
 }
 
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
-    [self.indicatorView stopAnimating];
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+    self.webView.hidden = NO;
+    [self.centerIndicatorView stopAnimating];
+    [self.cornerIndicatorView stopAnimating];
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+    [self.centerIndicatorView stopAnimating];
+    [self.cornerIndicatorView stopAnimating];
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Error" message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
     __weak typeof(self) welf = self;
     [alertController addAction:[UIAlertAction actionWithTitle:@"Dismiss" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
